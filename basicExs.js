cf8ae14e937af5ad1737148e5fbf0e2fa9feee74
dragonINXS <davidmudd@outlook.com>
2018-08-18 21:38:21 -0400

    did a few basic exercises

--- basicExs.js
+++ basicExs.js
@@ -1,10 +1,42 @@
-// 25. Write a JavaScript program check if a given positive number is a multiple of 3 or a multiple of 7.
+// // 25. Write a JavaScript program check if a given positive number is a multiple of 3 or a multiple of 7.
 
-function isDivBy3and7(num){
-    if(num % 3 === 0 || num % 7 === 0){
-      return true;
-    }
-    else { return false; }
-}
+// function isDivBy3and7(num){
+//     if(num % 3 === 0 || num % 7 === 0){
+//       return true;
+//     }
+//     else { return false; }
+// }
+
+// isDivBy3and7();
+
+
+// =============================================================================================
+// 1. Write a JavaScript function that reverse a number. 
+// Example x = 32243;
+// Expected Output : 34223
+
+// function revNum(num) {
+//     let x = num;
+//     return Number(String(x).split("").reverse().join(""));
+// }
   
-isDivBy3and7();
+// revNum(659);
+
+
+// ============================================================================================
+//  2. Write a JavaScript function that checks whether a passed string is palindrome or not? Go to the editor
+// A palindrome is word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run.
+
+// function isPalindrome(str){
+//     let revStr = str.split("").reverse().join("");
+    
+//     if (str.toLowerCase() === revStr.toLowerCase()) {
+//       return true;
+//     }
+//     else{return false;}
+// }
+  
+// isPalindrome("Bob");
+
+
+//=============================================================================================

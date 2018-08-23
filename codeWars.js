9da905708e9febe9d46fe6fcae5f31e609912fa0
dragonINXS <davidmudd@outlook.com>
2018-08-22 22:02:48 -0400

    did a codewars summation puzzle

--- codeWars.js
+++ codeWars.js
@@ -29,5 +29,45 @@ function maskify(cc) {
     return cc.slice(0, -4).replace(/./g, '#') + cc.slice(-4);
 }
 
-// ==================================================================================
-// ==================================================================================
+
+
+// =========================================================================================================
+// =========================================================================================================
+
+
+
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+// Rules:
+//     You need to round the answer to 2 decimal places and return it as String.
+//     If the given value is 0 then it should return 0.00
+//     You will only be given Natural Numbers as arguments.
+
+// Examples:
+
+// SeriesSum(1) => 1 = "1.00"
+// SeriesSum(2) => 1 + 1/4 = "1.25"
+// SeriesSum(5) => 1 + 1/4 + 1/7 + 1/10 + 1/13 = "1.57"
+
+// MY SOLUTION
+function SeriesSum(n) {
+    let total = 1;
+    let dnom = 4;
+    if (n === 0) { return 0..toFixed(2); }
+    while(n > 1) {
+      total += (1 / dnom);
+      dnom += 3;
+      n--;
+    }
+    return total.toFixed(2);
+}
+
+// BEST SOLUTION
+function SeriesSum(n) {
+    for (var s = 0, i = 0; i < n; i++) {
+        s += 1 / (1 + i * 3);
+    }
+    
+    return s.toFixed(2);
+}

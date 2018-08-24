e22bb21c02d4cb45b23444450d4ba10f1e672c8c
dragonINXS <davidmudd@outlook.com>
2018-08-23 22:57:38 -0400

    started a codewars but didn't finish yet

--- codeWars.js
+++ codeWars.js
@@ -71,3 +71,36 @@ function SeriesSum(n) {
     
     return s.toFixed(2);
 }
+
+// ==========================================================================================================
+
+// Count the number of Duplicates
+
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+// Example
+
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (bandB)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+
+
+function duplicates(str) {
+    return str.toLowerCase().split("").sort();   
+   
+    for (let i, dup = 0; sorted[i + 1] === sorted[i];){
+        
+    }
+   }
+   
+   // for (var i = 0; i < sorted_arr.length - 1; i++) {
+   //     if (sorted_arr[i + 1] == sorted_arr[i]) {
+   //         results.push(sorted_arr[i]);
+   //     }
+   
+   
+duplicates("Hello all");

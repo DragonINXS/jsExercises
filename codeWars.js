2649ec18b2b78f4b3e91c1bba20ec81ff9d634ca
dragonINXS <davidmudd@outlook.com>
2018-09-23 22:23:28 -0400

    started a codewars problem

--- codeWars.js
+++ codeWars.js
@@ -180,25 +180,6 @@ sayIt(4422666);
 // ==============================================================================================================
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Consecutive strings
 
 // You are given an array strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
@@ -213,4 +194,31 @@ sayIt(4422666);
 
 function consec(strArr, k ) {
 
+}
+
+
+// ==============================================================================================================
+
+
+// Largest 5 digit number in a series
+
+// In the following 6 digit number:
+
+// 283910
+
+// 91 is the greatest sequence of 2 consecutive digits.
+
+// In the following 10 digit number:
+
+// 1234567890
+
+// 67890 is the greatest sequence of 5 consecutive digits.
+
+// Complete the solution so that it returns the greatest sequence of five consecutive digits found within the number given. The number will be passed in as a string of only digits. It should return a five digit integer. The number passed may be as large as 1000 digits.
+
+// Adapted from ProjectEuler.net
+
+
+function largest5() {
+    
 }
